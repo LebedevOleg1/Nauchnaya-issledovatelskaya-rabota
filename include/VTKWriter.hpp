88efb51dbe9811bb9cf942ff01bf4ece8bffff46
Олег <oleglebedev222@gmail.com>
2025-05-20 23:29:45 +0300

    теперь точно работает

--- include/VTKWriter.hpp
+++ include/VTKWriter.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include <string>   // Добавлено
+#include <fstream>  // Добавлено
 #include "Mesh.hpp"
-#include <fstream>
 
 class VTKWriter {
 public:
@@ -8,24 +9,30 @@ public:
         std::string filename = "output_" + std::to_string(step) + ".vtr";
         std::ofstream file(filename);
 
+        // Остальной код без изменений
+        const int nx = mesh->get_nx();
+        const Float3 vmin = mesh->get_vmin();
+
         file << "<?xml version=\"1.0\"?>\n";
         file << "<VTKFile type=\"RectilinearGrid\" version=\"0.1\">\n";
-        file << "<RectilinearGrid WholeExtent=\"0 " << mesh->get_nx()-1 << " 0 0 0 0\">\n";
-        file << "<Piece Extent=\"0 " << mesh->get_nx()-1 << " 0 0 0 0\">\n";
+        file << "<RectilinearGrid WholeExtent=\"0 " << nx-1 << " 0 0 0 0\">\n";
+        file << "<Piece Extent=\"0 " << nx-1 << " 0 0 0 0\">\n";
+        
         file << "<PointData Scalars=\"Temperature\">\n";
         file << "<DataArray Name=\"Temperature\" type=\"Float32\" format=\"ascii\">\n";
-
-        float* T = mesh->get_T_curr();
-        for (int i = 0; i < mesh->get_nx(); ++i) {
-            file << T[i] << " ";
-        }
-
-        file << "\n</DataArray>\n</PointData>\n<Coordinates>\n";
+        for(int i = 0; i < nx; ++i) file << mesh->get_T_curr()[i] << " ";
+        file << "\n</DataArray>\n</PointData>\n";
+        
+        file << "<Coordinates>\n";
         file << "<DataArray type=\"Float32\" Name=\"X\">\n";
-        for (int i = 0; i < mesh->get_nx(); ++i) {
-            // Используем геттер вместо прямого доступа
-            file << mesh->get_vmin().x + (i + 0.5f) * mesh->get_hx() << " ";
-        }
-        file << "\n</DataArray>\n</Coordinates>\n</Piece>\n</RectilinearGrid>\n</VTKFile>";
+        for(int i = 0; i < nx; ++i) 
+            file << vmin.x + (i + 0.5f)*mesh->get_hx() << " ";
+        file << "\n</DataArray>\n";
+        
+        file << "<DataArray type=\"Float32\" Name=\"Y\">\n0.0\n</DataArray>\n";
+        file << "<DataArray type=\"Float32\" Name=\"Z\">\n0.0\n</DataArray>\n";
+        file << "</Coordinates>\n";
+        
+        file << "</Piece>\n</RectilinearGrid>\n</VTKFile>";
     }
 };
